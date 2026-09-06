10da39f96dfa04b70f4dd18b7d77ed4326313f21
fastfile34 <fastfile34@gmail.com>
2026-09-06 08:01:19 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "3nbf4.com",
-    "zoneId": 11735912
+    "domain": "5gvci.com",
+    "zoneId": 11735927
 }
 self.lary = ""
-importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
